81c68763bb6470665d821dcd8dcd4bbee5459993
MicahMonfrini <ffrock905@gmail.com>
2017-05-26 21:11:00 -0500

    create get requests for specific item routes

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import bodyParser from "body-parser";
 const app = express();
 app.use(bodyParser.json());
 
+// GET REQUESTS FOR BASE ROUTES
+
 app.get("/contacts", (request, response) => {
   return response.json(contacts);
 });
@@ -22,6 +24,36 @@ app.get("/comments", (request, response) => {
 
 app.get("/products", (request, response) => {
   return response.json(products);
+});
+
+// GET REQUESTS FOR SPECIFIC ITEM ROUTES
+
+app.get("/contacts/:id", (request, response) => {
+  const foundContact = contacts.find((contact) => {
+    return String(contact.id) === request.params.id;
+  });
+  return response.json(foundContact);
+});
+
+app.get("/vehicles/:id", (request, response) => {
+  const foundVehicle = vehicles.find((vehicle) => {
+    return String(vehicle.id) === request.params.id;
+  });
+  return response.json(foundVehicle);
+});
+
+app.get("/comments/:id", (request, response) => {
+  const foundComment = comments.find((comment) => {
+    return String(comment.id) === request.params.id;
+  });
+  return response.json(foundComment);
+});
+
+app.get("/products/:id", (request, response) => {
+  const foundProduct = products.find((product) => {
+    return String(product.id) === request.params.id;
+  });
+  return response.json(foundProduct);
 });
 
 const port = process.env.PORT || 3001;
