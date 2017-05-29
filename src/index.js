79a653caa6ab8cdb500c314b8a21e54a1deb7f9d
saf0911 <saf0911@gmail.com>
2017-05-29 08:11:01 -0500

    This is done

--- src/index.js
+++ src/index.js
@@ -3,10 +3,97 @@ import comments from "./comments";
 import products from "./products";
 import vehicle from "./vehicles";
 import contacts from "./contacts";
+import bodyParser from "body-parser";
 
 const app = express();
 
+app.use(bodyParser.json());
+
+
+// Comments section
+app.get("/comments", (request, response) => {
+  response.json(comments);
+});
+
+app.get("/comments/:id", (request, response) => {
+  const userComments = comments.find((comment) => {
+    return String(comment.id === request.params.id);
+  });
+  return response.json(userComments);
+});
+
+app.post("/comments", (request, response) => {
+  const addUserComment = {
+    id: comments.length + 1,
+    ...request.body
+  };
+  comments.push(request.body);
+  return response.json(addUserComment);
+});
+
+// app.delete("/comments", (request, response) => {
+//   const deleteUserComment = {
+//     id: comments.length - 1
+//   };
+//   comments.splice(request.body);
+//   return response.json(deleteUserComment);
+// });
+
+// Products section
+app.get("/products", (request, response) => {
+  response.json(products);
+});
+
+app.post("/products", (request, response) => {
+  const addUserProduct = {
+    id: products.length + 1,
+    ...request.body
+  };
+  products.push(request.body);
+  return response.json(addUserProduct);
+});
+
+// Vehicle section
+app.get("/vehicles", (request, response) => {
+  response.json(vehicle);
+});
+
+app.post("/vehicles", (request, response) => {
+  const addUserVehicle = {
+    id: vehicle.length + 1,
+    ...request.body
+  };
+  vehicle.push(request.body);
+  return response.json(addUserVehicle);
+});
+
+// Contacts section
+app.get("/contacts", (request, response) => {
+  response.json(contacts);
+});
+
+app.post("/contacts", (request, response) => {
+  const addUserContacts = {
+    id: contacts.length + 1,
+    ...request.body
+  };
+  contacts.push(request.body);
+  return response.json(addUserContacts);
+});
+
+
+
+
+app.get("/*", (request, response) => {
+  response.json({
+    Its: "Broke"
+  });
+});
+
 const port = process.env.PORT || 3001;
-app.listen(port, () => {
+app.listen(port, (err) => {
+  if (err) {
+    console.log("There was an error loading")
+  }
   console.log(`Listening on port:${port}`);
 });
