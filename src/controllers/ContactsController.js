61c308b176f24b3b2434937f9892b9a4bda050a9
MicahMonfrini <ffrock905@gmail.com>
2017-06-02 23:12:56 -0500

    create ContactController

---
+++ src/controllers/ContactsController.js
@@ -0,0 +1,40 @@
+import Contact from "../models/Contact";
+
+const ContactController = {
+
+  list: (request, response) => {
+    Contact.find().exec()
+      .then(data => {
+        return response.json(data);
+      })
+      .catch(() => {
+        return response.json("Error");
+      });
+  },
+
+  show: (request, response, next) => {
+    Contact.findById(request.params.id).exec()
+      .then((user) => {
+        console.log(response.json(user));
+      })
+      .catch((err) => {
+        next(err);
+      });
+  },
+
+  create: (request, response) => {
+    const contact = new Contact(request.body);
+
+    contact.save()
+      .then(storedContact => {
+        console.log("Contact was saved");
+        return response.json(storedContact);
+      })
+      .catch(() => {
+        console.log("Contact was NOT saved");
+        return response.json("Error");
+      });
+  }
+};
+
+export default ContactController;
--- src/routes/ContactsRouter.js
+++ src/routes/ContactsRouter.js
@@ -1,40 +1,12 @@
 import express from "express";
-import Contact from "../models/Contact";
+import ContactController from "../controllers/ContactController";
 
 const router = express.Router();
 
-router.get("/contacts", (request, response) => {
-  Contact.find().exec()
-    .then(data => {
-      return response.json(data);
-    })
-    .catch(() => {
-      return response.json("Error");
-    });
-});
+router.get("/contacts", ContactController.list);
 
-router.get("/contacts/:id", (request, response, next) => {
-  Contact.findById(request.params.id).exec()
-    .then((user) => {
-      console.log(response.json(user));
-    })
-    .catch((err) => {
-      next(err);
-    });
-});
+router.get("/contacts/:id", ContactController.show);
 
-router.post("/contacts", (request, response) => {
-  const contact = new Contact(request.body);
-
-  contact.save()
-    .then(storedContact => {
-      console.log("Contact was saved");
-      return response.json(storedContact);
-    })
-    .catch(() => {
-      console.log("Contact was NOT saved");
-      return response.json("Error");
-    });
-});
+router.post("/contacts", ContactController.create);
 
 export default router;
