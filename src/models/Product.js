b30dbd90e5292755e617fc47532c90e06a7adc8b
RX7RUSS <russ.savage5@gmail.com>
2017-06-03 20:13:06 -0500

    Product and Vehicle Models Complete

---
+++ src/models/Product.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+  name: String,
+  description: String,
+  reviewCount: Number,
+  rating: Number,
+  imgUrl: String,
+  price: String,
+  category: String,
+  reviews: Array
+});
+
+
+export default mongoose.model("Product", productSchema);
