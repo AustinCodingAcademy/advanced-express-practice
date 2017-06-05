a6a445f5a5e300a943448eb3c23c00efdc31e27f
RX7RUSS <russ.savage5@gmail.com>
2017-06-05 13:06:18 -0500

    get product action and all create actions finished

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -61,9 +61,9 @@ function vehiclesLoaded(vehicles) {
 
 
 export function getVehicle(id) {
-  return function(dispatch) {
+  return function (dispatch) {
     fetch("/vehicles/" + id)
-      .then((response => {
+      .then((response) => {
         return response.json();
       })
       .then((vehicle) => {
@@ -78,5 +78,119 @@ function getVehicleDone(vehicle) {
   return {
     type: "GET_VEHICLE_DONE",
     value: vehicle
+  };
+}
+
+export function loadComments() {
+  return function (dispatch) {
+    fetch("/comments")
+    .then( (response) => {
+      return response.json();
+    }).then((comments) => {
+      dispatch(commentsLoaded(comments));
+    });
+  };
+}
+function commentsLoaded(comments) {
+  return {
+    type: "COMMENTS_LOADED",
+    value: comments
+  };
+}
+
+export function getComment(id) {
+  return function (dispatch) {
+    fetch("/comments/" + id)
+      .then((response) => {
+        return response.json();
+      })
+      .then((comment) => {
+        dispatch(getCommentDone(comment));
+      })
+      .catch((err) => {
+        return console.log(err);
+      });
+  };
+}
+function getCommentDone(comment) {
+  return {
+    type: "GET_COMMENT_DONE",
+    value: comment
+  };
+}
+
+export function loadProducts() {
+  return function (dispatch) {
+    fetch("/products")
+    .then( (response) => {
+      return response.json();
+    }).then((products) => {
+      dispatch(productsLoaded(products));
+    });
+  };
+}
+function productsLoaded(products) {
+  return {
+    type: "PRODUCTS_LOADED",
+    value: products
+  };
+}
+
+export function getProduct(id) {
+  return function (dispatch) {
+    fetch("/products/" + id)
+      .then((response) => {
+        return response.json();
+      })
+      .then((product) => {
+        dispatch(getProductDone(product));
+      })
+      .catch((err) => {
+        return console.log(err);
+      });
+  };
+}
+function getProductDone(product) {
+  return {
+    type: "GET_PRODUCT_DONE",
+    value: product
+  };
+}
+
+
+export function createProduct(product) {
+  return function (dispatch) {
+    fetch("/products", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(product)
+    }).then(() => dispatch(loadProducts()));
+  };
+}
+export function createContact(c) {
+  return function (dispatch) {
+    fetch("/contacts", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(c)
+    }).then(() => dispatch(loadContacts()));
+  };
+}
+export function createVehicle(v) {
+  return function (dispatch) {
+    fetch("/vehicles", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(v)
+    }).then(() => dispatch(loadVehicles()));
+  };
+}
+export function createComment(v) {
+  return function (dispatch) {
+    fetch("/comments", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(v)
+    }).then(() => dispatch(loadComments()));
   };
 }
