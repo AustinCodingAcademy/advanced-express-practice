1a150b41b3db59e61fd80729b1e8514e38a176fb
athensbird <popularwatson@gmail.com>
2017-06-05 11:34:31 -0500

    Finished all four parts

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import express from "express";
-import products from "./products";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import ContactRoute from "./Routes/ContactRoutes";
 import CommentRoute from "./Routes/CommentRoutes";
 import VehicleRoute from "./Routes/VehicleRoutes";
+import ProductRoute from "./Routes/ProductRoutes";
 
 const app = express();
 app.use(bodyParser.json());
 app.use(ContactRoute);
 app.use(CommentRoute);
 app.use(VehicleRoute);
+app.use(ProductRoute);
 
 const port = process.env.PORT || 3001;
 
@@ -41,18 +42,18 @@ app.use((err, req, res, next) => {
   return res.status(500).json({message: err.message});
 });
 
-app.get("/products", (req, res) => {
-  return res.json(products);
-});
-
-app.get("/products/:id", (req, res) => {
-  const targetedProduct = products.find((product) => {
-    return String(product._id) === req.params.id;
-  });
-  return res.json(targetedProduct);
-});
-
-app.post("/products", (req, res) => {
-  products.push(req.body);
-  return res.json(products);
-});
+// app.get("/products", (req, res) => {
+//   return res.json(products);
+// });
+//
+// app.get("/products/:id", (req, res) => {
+//   const targetedProduct = products.find((product) => {
+//     return String(product._id) === req.params.id;
+//   });
+//   return res.json(targetedProduct);
+// });
+//
+// app.post("/products", (req, res) => {
+//   products.push(req.body);
+//   return res.json(products);
+// });
