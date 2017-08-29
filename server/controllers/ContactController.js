faffed33de2ba47ab0d4d0a36cc5eb45a377dde1
stuartkilgore <stuart.kilgore@gmail.com>
2017-08-28 20:54:21 -0500

    stuff

---
+++ server/controllers/ContactController.js
@@ -0,0 +1,10 @@
+export function list(req, res) { return res.json([]) };
+
+export function show(req, res) { return res.json({id: req.params.id}) };
+
+export function  create(req, res) { return res.json({}) };
+
+export function  update(req, res) { return res.json({id: req.params.id}) };
+
+export function  remove(req, res) { return res.json({}) };
+
--- server/index.js
+++ server/index.js
@@ -3,8 +3,11 @@ import comments from "./comments";
 import products from "./products";
 import vehicle from "./vehicles";
 import contacts from "./contacts";
+import contactRoutes from "./routes/ContactRoutes";
 
 const app = express();
+
+app.use(contactRoutes);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
