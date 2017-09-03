a7ff3e2e476bfec51a00c2c66a545312307585d6
mymonstermaggie <maggie@remote-com.com>
2017-09-03 11:20:10 -0500

    created controllers and routes

---
+++ server/controllers/ContactsController.js
@@ -0,0 +1,26 @@
+import contacts from "../contacts";
+
+//GET => List
+export function list(request, response) {
+ return response.json(contacts);
+}
+
+//GET/:id => Show
+export function show(request, response) {
+ return response.json({theId: request.params.id});
+}
+
+//POST => Create
+export function create(request, response) {
+ return response.json({});
+}
+
+//PUT/:id => Update
+export function update(request, response) {
+ return response.json({theId: request.params.id});
+}
+
+//DELETE/:id => Remove
+export function remove(request, response) {
+ return response.json({});
+}
--- server/index.js
+++ server/index.js
@@ -1,12 +1,28 @@
 import express from "express";
-import comments from "./comments";
-import products from "./products";
-import vehicle from "./vehicles";
-import contacts from "./contacts";
+import bodyParser from "body-parser";
+
+import commentsRoutes from "./routes/CommentsRoutes";
+import contactsRoutes from "./routes/ContactsRoutes";
+import productsRoutes from "./routes/ProductsRoutes";
+import vehiclesRoutes from "./routes/VehiclesRoutes";
+
 
 const app = express();
+app.use(bodyParser.json());
+app.use(commentsRoutes);
+app.use(contactsRoutes);
+app.use(productsRoutes);
+app.use(vehiclesRoutes);
 
 const port = process.env.PORT || 3001;
+
+app.get("/", function (request, response) {
+  return response.json({hello: "get world"});
+});
+app.post("/", function (request, response) {
+  return response.json({hello: "post world"})
+})
+
 app.listen(port, () => {
   console.log(`Listening on port:${port}`);
 });
