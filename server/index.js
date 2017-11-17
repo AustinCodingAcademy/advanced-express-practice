0b611526d2fa7bed38ed0579964dd9bd61c1a510
Casey O'Keefe <the0surfer@aol.com>
2017-11-16 19:18:41 -0600

    get for contact not showing

--- server/index.js
+++ server/index.js
@@ -3,10 +3,16 @@ import comments from "./comments";
 import products from "./products";
 import vehicle from "./vehicles";
 import contacts from "./contacts";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Listening on port:${port}`);
 });
+
+app.get("/contacts", (request, response) => {
+  return response.json(contacts);
+ });
