1ff35512c4b7d9a5cae7f7ca450fd49592ea0a00
Andrew Hartford <andrewhartford@utexas.edu>
2017-11-24 16:27:13 -0600

    component updates

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -1,9 +1,19 @@
 import React from "react";
 import CollapsableMapper from "./CollapsableMapper";
 
-function Contacts(props) {
-  return (
-    <CollapsableMapper data={props.contacts} path="contact" field="name" />
-  );
+class Contact extends React.Component {
+  componentDidMount() {
+    var contactid = this.props.match.params.id
+    this.props.getContac(contactid);
+  }
+  render() {
+    return (
+      <div>
+        <div>this.props.contact.name</div>
+        <div>this.props.contact.occupation</div>
+      </div>
+    );
+  }
 }
+
 export default Contacts;
