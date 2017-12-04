c1255b21fcb0fbb720ec24a710169aca1cc1ca14
Mac User <macuser@macs-mbp.gateway.pace.com>
2017-12-03 19:38:26 -0800

    re-organize actions by category

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,36 +1,5 @@
-export function loadContacts() {
-  return function (dispatch) {
-    fetch("/contacts")
-    .then( (response) => {
-      return response.json();
-    }).then((contacts) => {
-      dispatch(contactsLoaded(contacts));
-    });
-  };
-}
-function contactsLoaded(contacts) {
-  return {
-    type: "CONTACTS_LOADED",
-    value: contacts
-  };
-}
+// COMMENTS
 
-export function loadVehicles() {
-  return function (dispatch) {
-    fetch("/vehicles")
-    .then( (response) => {
-      return response.json();
-    }).then((vehicles) => {
-      dispatch(vehiclesLoaded(vehicles));
-    });
-  };
-}
-function vehiclesLoaded(vehicles) {
-  return {
-    type: "VEHICLES_LOADED",
-    value: vehicles
-  };
-}
 export function loadComments() {
   return function (dispatch) {
     fetch("/comments")
@@ -47,49 +16,9 @@ function commentsLoaded(comments) {
     value: comments
   };
 }
-export function loadProducts() {
-  return function (dispatch) {
-    fetch("/products")
-    .then( (response) => {
-      return response.json();
-    }).then((products) => {
-      dispatch(productsLoaded(products));
-    });
-  };
-}
-function productsLoaded(products) {
-  return {
-    type: "PRODUCTS_LOADED",
-    value: products
-  };
-}
-export function createProduct(product) {
-  return function (dispatch) {
-    fetch("/products", {
-      method: "POST",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(product)
-    }).then(() => dispatch(loadProducts()));
-  };
-}
-export function createContact(c) {
-  return function (dispatch) {
-    fetch("/contacts", {
-      method: "POST",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(c)
-    }).then(() => dispatch(loadContacts()));
-  };
-}
-export function createVehicle(v) {
-  return function (dispatch) {
-    fetch("/vehicles", {
-      method: "POST",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(v)
-    }).then(() => dispatch(loadVehicles()));
-  };
-}
+
+// COMMENT SINGLE
+
 export function createComment(comment) {
   return function (dispatch) {
     fetch("/comments", {
@@ -97,44 +26,6 @@ export function createComment(comment) {
       headers: {"Content-Type": "application/json"},
       body: JSON.stringify(comment)
     }).then(() => dispatch(loadComments()));
-  };
-}
-
-// show just one
-
-export function getProduct(id) {
-  return function (dispatch) {
-    fetch(`/products/${id}`)
-    .then( (response) => {
-      return response.json();
-    }).then((product) => {
-      dispatch(getProductDone(product));
-    });
-  };
-}
-
-function getProductDone(product) {
-  return {
-    type: "GET_PRODUCT_DONE",
-    value: product
-  };
-}
-
-export function getVehicle(id) {
-  return function (dispatch) {
-    fetch(`/vehicles/${id}`)
-    .then( (response) => {
-      return response.json();
-    }).then((vehicle) => {
-      dispatch(getVehicleDone(vehicle));
-    });
-  };
-}
-
-function getVehicleDone(vehicle) {
-  return {
-    type: "GET_VEHICLE_DONE",
-    value: vehicle
   };
 }
 
@@ -156,6 +47,38 @@ function getCommentDone(comment) {
   };
 }
 
+// CONTACTS
+
+export function loadContacts() {
+  return function (dispatch) {
+    fetch("/contacts")
+    .then( (response) => {
+      return response.json();
+    }).then((contacts) => {
+      dispatch(contactsLoaded(contacts));
+    });
+  };
+}
+
+function contactsLoaded(contacts) {
+  return {
+    type: "CONTACTS_LOADED",
+    value: contacts
+  };
+}
+
+// CONTACT SINGLE
+
+export function createContact(c) {
+  return function (dispatch) {
+    fetch("/contacts", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(c)
+    }).then(() => dispatch(loadContacts()));
+  };
+}
+
 export function getContact(id) {
   return function (dispatch) {
     fetch(`/contacts/${id}`)
@@ -171,5 +94,105 @@ function getContactDone(contact) {
   return {
     type: "GET_CONTACT_DONE",
     value: contact
+  };
+}
+
+// PRODUCTS
+
+export function loadProducts() {
+  return function (dispatch) {
+    fetch("/products")
+    .then( (response) => {
+      return response.json();
+    }).then((products) => {
+      dispatch(productsLoaded(products));
+    });
+  };
+}
+
+function productsLoaded(products) {
+  return {
+    type: "PRODUCTS_LOADED",
+    value: products
+  };
+}
+
+// PRODUCT SINGLE
+
+export function createProduct(product) {
+  return function (dispatch) {
+    fetch("/products", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(product)
+    }).then(() => dispatch(loadProducts()));
+  };
+}
+
+export function getProduct(id) {
+  return function (dispatch) {
+    fetch(`/products/${id}`)
+    .then( (response) => {
+      return response.json();
+    }).then((product) => {
+      dispatch(getProductDone(product));
+    });
+  };
+}
+
+function getProductDone(product) {
+  return {
+    type: "GET_PRODUCT_DONE",
+    value: product
+  };
+}
+
+// VEHICLES
+
+export function loadVehicles() {
+  return function (dispatch) {
+    fetch("/vehicles")
+    .then( (response) => {
+      return response.json();
+    }).then((vehicles) => {
+      dispatch(vehiclesLoaded(vehicles));
+    });
+  };
+}
+
+function vehiclesLoaded(vehicles) {
+  return {
+    type: "VEHICLES_LOADED",
+    value: vehicles
+  };
+}
+
+// VEHICLE SINGLE
+
+export function createVehicle(v) {
+  return function (dispatch) {
+    fetch("/vehicles", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(v)
+    }).then(() => dispatch(loadVehicles()));
+  };
+}
+
+export function getVehicle(id) {
+  return function (dispatch) {
+    fetch(`/vehicles/${id}`)
+    .then( (response) => {
+      return response.json();
+    }).then((vehicle) => {
+      dispatch(getVehicleDone(vehicle));
+    });
+  };
+}
+
+function getVehicleDone(vehicle) {
+  return {
+    type: "GET_VEHICLE_DONE",
+    value: vehicle
   };
 }
