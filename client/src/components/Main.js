095e01f19fb1bd52ed5f6b3878e8abd944d54e2b
Andrew Hartford <andrewhartford@utexas.edu>
2017-12-28 20:17:05 -0600

    finished express

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -9,13 +9,13 @@ function Contacts() {
   return (
     <div>
       <div style={{float: "left", width: "49%"}}>
-        <h1>Comments </h1>
+        <h1>Commets </h1>
         <CommentsContainer />
         <h1>Contacts</h1>
         <ContactsContainer />
         <h1>Vehicles</h1>
         <VehiclesContainer />
-        <h1>Products</h1>
+        <h1>Productss</h1>
         <ProductsContainer />
       </div>
       <div style={{float: "left", width: "49%"}}>
