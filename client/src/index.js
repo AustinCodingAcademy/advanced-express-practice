8d148c8b0b5436797823ba374d8b8c068b83973a
Andrew Hartford <andrewhartford@utexas.edu>
2017-12-28 20:25:30 -0600

    first database commit

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import AppContainer from "./AppContainer";
 import "./index.css";
 import store from "./store";
 import {Provider} from "react-redux";
+import mongoose from "mongoose";
 
 ReactDOM.render(
   <Provider store={store}><AppContainer /></Provider>,
