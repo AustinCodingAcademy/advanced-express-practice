6f6feae6b6497f65767e05d733b0b1212aee74a7
unknown <geohalbert@gmail.com>
2018-03-03 03:42:36 -0600

    part 1 done

--- server/index.js
+++ server/index.js
@@ -1,10 +1,63 @@
 import express from "express";
+import bodyParser from "body-parser";
 import comments from "./comments";
 import products from "./products";
 import vehicles from "./vehicles";
 import contacts from "./contacts";
 
 const app = express();
+app.use(bodyParser.json());
+
+//snippets made for expressgetroute and expresspostroute
+
+app.get("/contacts",function (request,response,next){
+ response.json(contacts);
+});
+app.get("/contacts/:id",function (request,response,next){
+ response.json(contacts.find((c)=>c._id==request.params.id));
+});
+app.post("/contacts",function (request,response,next){
+ let newContact = request.body
+ contacts.push(newContact);
+ response.json(newContact);
+});
+
+
+app.get("/vehicles",function (request,response,next){
+ response.json(vehicles);
+});
+app.get("/vehicles/:id", function (request,response,next){
+  response.json(vehicles.find((c)=>c._id==request.params.id));
+});
+app.post("/vehicles", function (request,response,next){
+  let newVehicle = request.body;
+  vehicles.push(newVehicle);
+  response.json(newVehicle);
+});
+
+app.get("/comments",function (request,response,next){
+ response.json(comments);
+});
+app.get("/comments/:id",function (request,response,next){
+ response.json(comments.find((c)=>c._id==request.params.id));
+});
+app.post("/comments", function (request,response,next){
+  let newComment = request.body;
+  comments.push(newComment);
+  response.json(newComment);
+});
+
+app.get("/products",function (request,response,next){
+ response.json(products);
+});
+app.get("/products/:id",function (request,response,next){
+ response.json(products.find((c)=>c._id==request.params.id));
+});
+app.post("/products", function (request,response,next){
+  let newProduct = request.body;
+  products.push(newProduct);
+  response.json(newProduct);
+});
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
