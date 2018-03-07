42021551347eb9028b9284f257c5d73ea031f75f
Neil Brownell <neil@onnit.com>
2018-03-07 16:55:25 -0600

    Change server to port 5000 because to avoid conflict with something unrelated I have running on port 3000

--- server/index.js
+++ server/index.js
@@ -6,7 +6,11 @@ import contacts from "./contacts";
 
 const app = express();
 
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
+const port = process.env.PORT || 5001;
+app.listen(port, (err) => {
   console.log(`Listening on port:${port}`);
+  if (err) {
+    return console.log("Error", err);
+  }
+});
 });
