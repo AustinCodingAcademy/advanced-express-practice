1288a7b1da089cc4d42e9657d4a26ebcc76aaa06
walzer85 <walzer85@gmail.com>
2018-05-01 20:14:36 -0500

    added the .get routing

--- server/index.js
+++ server/index.js
@@ -4,3 +4,27 @@ let products = require("./products");
 let vehicles = require("./vehicles");
 let contacts = require("./contacts");
 
+const app = express();
+
+app.listen(3001, (err) => {
+if (err) {
+    return console.log("Error", err);
+}
+console.log("Web server is now living in apartment 3001");
+});
+
+app.get("/contacts",function(req,res,next){
+    return res.send(contacts});
+});
+
+app.get("/vehicles",function(req,res,next){
+    return res.send(vehicles);
+});
+
+app.get("/comments",function(req,res,next){
+    return res.send(comments);
+});
+
+app.get("/products",function(req,res,next){
+    return res.send(products);
+});
