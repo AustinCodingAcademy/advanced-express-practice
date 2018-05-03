a2832e5243dde8d8adc5426b61c6aef64906ffad
Ty Sweet <tysweet77@gmail.com>
2018-05-03 06:40:21 -0500

    added controllers and routes

--- server/index.js
+++ server/index.js
@@ -1,71 +1,15 @@
 const express = require("express");
-const comments = require("./comments");
-const products = require("./products");
-const vehicles = require("./vehicles");
-const contacts = require("./contacts");
-const bodyParser = require("body-parser");
-
 const app = express();
-
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-
-
-// Get all things
-app.get("/contacts",function (req,res,next) {
-  return res.json(contacts);
-});
-
-app.get("/vehicles",function (req,res,next) {
-  return res.json(vehicles);
-});
-
-app.get("/comments",function (req,res,next) {
-  return res.json(comments);
-});
-
-app.get("/products",function (req,res,next) {
-  return res.json(products);
-});
-
-
-// Get one thing - id
-app.get("/contact/:id",function (req,res,next) {
-  const contactId = contacts.find(user => user._id == req.params.id);
-  return res.json(contactId);
-});
-  
-app.get("/vehicle/:id",function (req,res,next) {
-  const vehicleId = vehicles.find(user => user._id == req.params.id);
-  return res.json(vehicleId);
-});
-  
-app.get("/comment/:id",function (req,res,next) {
-  const commentId = comments.find(user => user._id == req.params.id);
-  return res.json(commentId);
-});
-  
-app.get("/product/:id",function (req,res,next) {
-  const productId = products.find(user => user._id == req.params.id);
-  return res.json(productId);
-});
-
-
-// Create one thing
-app.post("/contacts",function (req,res,next) {
-  return res.json(contacts.push({}));
-});
-
-app.post("/vehicles",function (req,res,next) {
-  return res.json(vehicles.push({}));
-});
-
-app.post("/comments",function (req,res,next) {
-  return res.json(comments.push({}));
-});
-
-app.post("/products",function (req,res,next) {
-  return res.json(products.push({}));
-});
+const ContactRoutes = require("./routes/ContactRoutes");
+app.use(ContactRoutes);
+const CommentRoutes = require("./routes/CommentRoutes");
+app.use(CommentRoutes);
+const ProductRoutes = require("./routes/ProductRoutes");
+app.use(ProductRoutes);
+const VehicleRoutes = require("./routes/VehicleRoutes");
+app.use(VehicleRoutes);
 
 
 // Listening to port 3001
