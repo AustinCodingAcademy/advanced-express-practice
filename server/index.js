cd40fbb0ec4c6780943ebf78094a11e8ad824aa2
Matt Cameron <cameron.matt@gmail.com>
2018-05-05 14:54:38 -0500

    centralized routes in routes/index.js

--- server/index.js
+++ server/index.js
@@ -1,19 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const ContactRoutes = require("./routes/ContactRoutes");
-const CommentRoutes = require("./routes/CommentRoutes");
-const ProductRoutes = require("./routes/ProductRoutes");
-const VehicleRoutes = require("./routes/VehicleRoutes");
-
 const app = express();
+
 app.use(bodyParser.json());
-
-app.use(ContactRoutes);
-app.use(CommentRoutes);
-app.use(ProductRoutes);
-app.use(VehicleRoutes);
-
+app.use(require("./routes"));
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
