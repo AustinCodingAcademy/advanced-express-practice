afdcfa54966626620554d6579c270b98e3984b14
walzer85 <walzer85@gmail.com>
2018-05-05 07:28:40 -0500

    corrected the routes in index.js

--- server/index.js
+++ server/index.js
@@ -1,72 +1,24 @@
-let express = require("express");
-let comments = require("./comments");
-let products = require("./products");
-let vehicles = require("./vehicles");
-let contacts = require("./contacts");
-let bodyParser = require("body-parser");
-let state = require("./state")
-let users = require(state.users);
-
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+
 app.use(bodyParser.json());
 
-app.listen(5555, (err) => {
+app.listen(3001, (err) => {
 if (err) {
     return console.log("Error", err);
 }
 console.log("Web server is now living in apartment 3001");
 });
 
-app.get("/contacts",function(req,res,next){
-    return res.send(contacts);
-});
+const ContactRoutes = require("./routes/ContactRoutes");
+app.use(ContactRoutes);
 
-app.get("/vehicles",function(req,res,next){
-    return res.send(vehicles);
-});
+const CommentRoutes = require("./routes/CommentRoutes");
+app.use(CommentRoutes);
 
-app.get("/comments",function(req,res,next){
-    return res.send(comments);
-});
+const ProductRoutes = require("./routes/ProductRoutes");
+app.use(ProductRoutes);
 
-app.get("/products",function(req,res,next){
-    return res.send(products);
-});
-
-//need to add the correct response.json
-
--app.get("/contacts/:id",function (req,res,next){	
-    const contactId = contacts.find(user => user._id == req.params.id);	
-    return res.json(contactId);	
-});
-
--app.get("/vehicles/:id",function (req,res,next){	
-    const vehicleId = vehicles.find(user => user._id == req.params.id);	
-    return res.json(vehicleId);	
-});
-
--app.get("/comments/:id",function (req,res,next){	
-    const commentId = contacts.find(user => user._id == req.params.id);	
-    return res.json(commentId);	
-});
-
--app.get("/products/:id",function (req,res,next){	
-    const productId = products.find(user => user._id == req.params.id);	
-    return res.json(productId);	
-});
-
-app.post("/contacts", function (req, res, next){
-    return res.json(contacts.push({}));
-});
-
-app.post("/vehicles", function (req, res, next){
-    return res.json(vehicles.push({}));
-});
-
-app.post("/comments", function (req, res, next){
-    return res.json(comments.push({}));
-});
-
-app.post("/products", function (req, res, next){
-    return res.json(products.push({}));
-});
+const VehicleRoutes = require("./routes/VehicleRoutes");
+app.use(VehicleRoutes);
