ea45ed3a235565b9695ddb419b2ef3fda040fda2
walzer85 <walzer85@gmail.com>
2018-05-07 17:52:11 -0500

    commented out mongoose stuff for current grading functionality

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 
 
 app.use(bodyParser.json());
-mongoose.connect('mongodb://eric.walz:@thedr1vein@ds115350.mlab.com:15350/advanced-express-practice');
+// mongoose.connect('mongodb://eric.walz:@thedr1vein@ds115350.mlab.com:15350/advanced-express-practice');
 
 // var db = mongoose.connection;
 // db.on('error', console.error.bind(console, 'connection error:'));
