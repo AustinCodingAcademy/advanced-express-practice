3d96b4cad4a68b07e44918123a90b46e0630bb63
SeanPhilippi <sean.philippi@gmail.com>
2018-11-01 17:52:34 -0500

    comments added

--- server/routes/ContactRoutes.js
+++ server/routes/ContactRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+// destructuring so require("<controller route path>") doesn't have to be repeated
 const {list, show, create, update, remove} = require(
    "../controllers/ContactController");
 
