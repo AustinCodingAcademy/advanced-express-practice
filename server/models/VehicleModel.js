bb662b0b18cfdeb0f4e159ffad9bdcd9426f7325
SeanPhilippi <sean.philippi@gmail.com>
2018-11-12 14:27:51 -0600

    model schemas created

--- server/models/VehicleModel.js
+++ server/models/VehicleModel.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 const vehicleSchema = new mongoose.Schema({
   year: { type: String },
   make: { type: String },
-  model: { type: String },
-  price: { type: String },
+  model: { type: String }
 });
 
 module.exports = mongoose.model("Vehicle", vehicleSchema);
