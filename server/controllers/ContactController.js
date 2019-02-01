c257edd9a36cc5fa68255547a05cdfb27bbcdcdc
Christopher Willis <willis.christopher.a@gmail.com>
2019-01-31 22:24:36 -0600

    many changes

---
+++ server/controllers/ContactController.js
@@ -0,0 +1,18 @@
+const contacts = require("../contacts");
+
+
+exports.list = function list(request, response) {
+  return response.json(contacts);
+}
+exports.show = function show(request, response) {
+  return response.json({theId: request.params.id});
+}
+exports.create = function create(request, response) {
+  return response.json({});
+}
+exports.update = function update(request, response) {
+  return response.json({theId: request.params.id});
+}
+exports.remove = function remove(request, response) {
+  return response.json({});
+}
