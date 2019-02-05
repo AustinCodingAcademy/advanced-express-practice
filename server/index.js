c6553c57534689bb399cdfbe41e3cda680b3f0c5
unknown <erlanggaramzy@gmail.com>
2019-02-05 12:41:31 -0600

    initial server pull

--- server/index.js
+++ server/index.js
@@ -1,6 +1,24 @@
-let express = require("express");
-let comments = require("./comments");
-let products = require("./products");
-let vehicles = require("./vehicles");
-let contacts = require("./contacts");
+const express = require("express");
+const comments = require("./comments");
+const products = require("./products");
+const vehicles = require("./vehicles");
+const contacts = require("./contacts");
 
+const bodyParser = require("body-parser");
+const contactRoutes = require("./routes/ContactRoutes");
+const vehicleRoutes = require("./routes/VehicleRoutes");
+const productRoutes = require("./routes/ProductRoutes");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(contactRoutes);
+app.use(vehicleRoutes);
+app.use(productRoutes);
+
+app.listen(3001, (err) => {
+    if (err) {
+      return console.log("Error", err);
+    }
+    console.log("Web server is now listening for messages", err);
+});
+   
