db6d328b08b8bf8a82312724b54e3fdc16437314
User <campa813@gmail.com>
2019-02-06 17:14:58 -0600

    Using express routes for multiple items.

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-let express = require("express");
-let comments = require("./comments");
-let products = require("./products");
-let vehicles = require("./vehicles");
-let contacts  = require("./contacts");
+const express = require("express");
+
+//Routes
+const commentRoutes = require("./server/routes/CommentRoutes"); 
+const contactRoutes = require("./server/routes/ContactRoutes");
+const vehicleRoutes = require("./server/routes/VehicleRoutes");
+const productRoutes = require("./server/routes/ProductRoutes");
 
 const bodyParser = require("body-parser");
 const app = express();
+
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
-const thePort = 3001;
+app.use(commentRoutes); 
+app.use(contactRoutes);
+app.use(vehicleRoutes);
+app.use(productRoutes);
 
+const thePort = 3001;
 
 app.listen(thePort, (err) => {
  if (err) {
    return console.log("Error", err);
  }
- console.log("Web server is now listening for messages on port",thePort);
+ console.log("Web server is now listening for messages on port", thePort);
 });
