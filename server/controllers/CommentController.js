64f8698894e99781c7384b5de13a2bdfa8a7de13
Abby Gottlich <abby.gottlich@gmail.com>
2019-02-09 08:22:40 -0600

    attempted functions - not working yet

--- server/controllers/CommentController.js
+++ server/controllers/CommentController.js
@@ -1,21 +1,28 @@
-let comments = require("../comments");
+const Comment = require("./models/CommentModel");
 
 // GET request
-exports.list = function list(request, response) {
-    return response.json(comments);
+// exports.list = function list(request, response) {
+//     return response.json(Comment);
+// }
+function findAll(done) {
+    Comment.find(function (err, comments) {
+        return (comments);
+    });
+    done();
 }
+module.exports = findAll;
 
 // GET with an ID
 exports.show = function show(request, response) {
-    return response.json(comments[request.params.id - 1]);
+    return response.json(Comment[request.params.id - 1]);
 }
 
 // POST request
 exports.create = function create(request, response) {
     const newComment = request.body;
-    newComment["_id"] = comments.length + 1;
-    comments.push(newComment);
-    return response.json(comments);
+    newComment["_id"] = Comment.length + 1;
+    Comment.push(newComment);
+    return response.json(Comment);
 }
 
 // PUT request
