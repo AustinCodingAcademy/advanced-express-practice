d71f2dc3ccb286ab751e8ac9f7a6e586c4fa08f1
Darwin Risser <riser.darwin@gmail.com>
2019-02-10 21:35:38 -0600

    testing comments again

--- server/controllers/CommentsController.js
+++ server/controllers/CommentsController.js
@@ -5,19 +5,27 @@ exports.list = function list(request, response) {
         return response.json(comments);
     })
 }
-   exports.show = function show(request, response) {
-    const viewComment = comments.find(c=>c._id == request.params.id);
-    return response.json(viewComment);
-   }
-   exports.create =  function create(request, response) {
-    console.log('creating stuff')
-    const newComment = request.body;
-    comments.push(newComment);
-    return response.json(newComment);
-   }
-   exports.update =  function update(request, response) {
+
+exports.show = function show(request, response) {
+    Comment.findById(request.params.id).exec().then((product)=> {
+        return response.json(product);
+    })
+}
+
+exports.create =  function create(request, response) {
+    
+    const newComment = new Comment({
+        body: request.body
+    })
+    newComment.save.then(savedComment=>{
+        console.log(savedComment)
+    })    
+}
+
+exports.update =  function update(request, response) {
     return response.json({theId: request.params.id});
-   }
-   exports.remove =  function remove(request, response) {
+}
+
+exports.remove =  function remove(request, response) {
     return response.json({});
    }
