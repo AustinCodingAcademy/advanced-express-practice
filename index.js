8c29dc587330028dbc9be58abf9523b1ead5677c
unknown <collinb512@gmail.com>
2019-05-17 15:12:11 -0500

    updates

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
     
 let express = require("express");
+let mongoose = require('mongoose')
 
 const app = express();
 const port = 3001;
@@ -12,6 +13,9 @@ const contactsRoute = require('./routes/contacts-r');
 const commentsRoute = require('./routes/comments-r');
 const productsRoute = require('./routes/products-r');
 const vehiclesRoute = require('./routes/vehicles-r');
+
+mongoose.connect("mongodb+srv://cbetori:betori12@database-cb-nio9a.gcp.mongodb.net/advanced-express-practice?retryWrites=true",{useNewUrlParser: true})
+
 
 app.use(contactsRoute, commentsRoute, productsRoute, vehiclesRoute);
 
