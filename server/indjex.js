c10c5fc920f62bd4964dc8c372fc4bf4263419c7
Andrew Sommere <andrew.conrad.sommer@gmail.com>
2019-06-18 10:55:00 -0500

    part 4 done

--- server/indjex.js
+++ server/indjex.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
 
 const MongoClient = require(‘mongodb’).MongoClient;
-const uri = "mongodb+srv://asommer:QAZwsx123@database-server-1-5wzy9.mongodb.net/test?retryWrites=true";
+const uri = "mongodb+srv://user1:12345@database-server-1-5wzy9.mongodb.net/test?retryWrites=true";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
+  const collection = client.db("advanced-express-practice").collection("devices");
   // perform actions on the collection object
   client.close();
 });
