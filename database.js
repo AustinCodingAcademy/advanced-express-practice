a1c2e5c5cd11ed3125a42e8ee60453bc58b2295c
dbobb220 <david.bobb.220@gmail.com>
2019-07-30 09:27:35 -0500

    database working

--- database.js
+++ database.js
@@ -1,19 +1,21 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 const dotenv = require('dotenv').config();
+
+
  
-// Connection URL
+// change this to your mongodb atlas uri
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@aca-practice-bx4sb.mongodb.net/test?retryWrites=true&w=majority`;
- 
-// Database Name
+const client = new MongoClient(url);
 const dbName = 'advanced-express-practice';
- 
+
+let areYouConnected = (err) => {
+    if (err) {
+        console.log(err);
+        return
+    }
+    console.log('Connected successfully to server')
+}
+
 // Use connect method to connect to the server
-MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
- 
-  const db = client.db(dbName);
- 
-  client.close();
-});
+client.connect(areYouConnected, { useNewUrlParser: true })
