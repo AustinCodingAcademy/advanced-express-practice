2cc26a7027402b704c98c1bc4cff1d2d84a0b800
Joseph William Hauger <joseph>
2019-07-29 20:54:29 -0500

    mongoose begins

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const contactRoute = require("./Routes/Contact_routes");
 const vehicleRoute = require("./Routes/Vehicles_routes");
 const commentRoute = require("./Routes/Comments_routes");
 const productRoute = require("./Routes/Products_routes");
 const thePort = 3001;
+
+mongoose.connect('mongodb://localhost/advanced-express-practice')
 
 const bodyParser = require("body-parser");
 const app = express();
